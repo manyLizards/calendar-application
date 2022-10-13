f2c740b74a757cfc21acdf3be2b957efa45e4fbf
Jordan <jnbarringer96@gmail.com>
2022-10-12 20:29:23 -0500

    attempted to color code time blocks with a for loop - unsuccessful but may come back to this commit to try again

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // grab the current time exact
-var currentDate = moment();
+var currentDate = moment().local();
 
-// grab html elements
+// grab Text elements
 var eight = document.querySelector("#eight");
 var nine = document.querySelector("#nine");
 var ten = document.querySelector("#ten");
@@ -17,135 +17,31 @@ var five = document.querySelector("#five");
 $('#currentDay').text(currentDate.format("ddd, MMM, Do, YYYY"));
 
 // Set variable for current hour
-var currentTime = currentDate.format("H");
+var currentTime = currentDate.format("h");
 
-console.log(currentTime)
+console.log(eight.innerText)
 
-// Attempt to code time block by time
-//Attempt #1 - associated with lines 5-14
-// v This code is not working v
-//function colorCode () {
-//     if (currentTime === 8) {
-//         eight.style.backgroundColor = green;
-//         nine.style.backgroundColor = yellow;
-//         ten.style.backgroundColor = yellow;
-//         eleven.style.backgroundColor = yellow;
-//         twelve.style.backgroundColor = yellow;
-//         one.style.backgroundColor = yellow;
-//         two.style.backgroundColor = yellow;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 9) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = green;
-//         ten.style.backgroundColor = yellow;
-//         eleven.style.backgroundColor = yellow;
-//         twelve.style.backgroundColor = yellow;
-//         one.style.backgroundColor = yellow;
-//         two.style.backgroundColor = yellow;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 10) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = green;
-//         eleven.style.backgroundColor = yellow;
-//         twelve.style.backgroundColor = yellow;
-//         one.style.backgroundColor = yellow;
-//         two.style.backgroundColor = yellow;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 11) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = green;
-//         twelve.style.backgroundColor = yellow;
-//         one.style.backgroundColor = yellow;
-//         two.style.backgroundColor = yellow;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 12) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = gray;
-//         twelve.style.backgroundColor = green;
-//         one.style.backgroundColor = yellow;
-//         two.style.backgroundColor = yellow;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 1) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = gray;
-//         twelve.style.backgroundColor = gray;
-//         one.style.backgroundColor = green;
-//         two.style.backgroundColor = yellow;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 2) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = gray;
-//         twelve.style.backgroundColor = gray;
-//         one.style.backgroundColor = gray;
-//         two.style.backgroundColor = green;
-//         three.style.backgroundColor = yellow;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 3) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = gray;
-//         twelve.style.backgroundColor = gray;
-//         one.style.backgroundColor = gray;
-//         two.style.backgroundColor = gray;
-//         three.style.backgroundColor = green;
-//         four.style.backgroundColor = yellow;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 4) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = gray;
-//         twelve.style.backgroundColor = gray;
-//         one.style.backgroundColor = gray;
-//         two.style.backgroundColor = gray;
-//         three.style.backgroundColor = gray;
-//         four.style.backgroundColor = green;
-//         five.style.backgroundColor = yellow;
-//     } else if (currentTime === 5) {
-//         eight.style.backgroundColor = gray;
-//         nine.style.backgroundColor = gray;
-//         ten.style.backgroundColor = gray;
-//         eleven.style.backgroundColor = gray;
-//         twelve.style.backgroundColor = gray;
-//         one.style.backgroundColor = gray;
-//         two.style.backgroundColor = gray;
-//         three.style.backgroundColor = gray;
-//         four.style.backgroundColor = gray;
-//         five.style.backgroundColor = green;
-//     } else {
-//             eight.style.backgroundColor = gray;
-//             nine.style.backgroundColor = gray;
-//             ten.style.backgroundColor = gray;
-//             eleven.style.backgroundColor = gray;
-//             twelve.style.backgroundColor = gray;
-//             one.style.backgroundColor = gray;
-//             two.style.backgroundColor = gray;
-//             three.style.backgroundColor = gray;
-//             four.style.backgroundColor = gray;
-//             five.style.backgroundColor = gray;
-//         }
-//     }
-// ^ This code is not working ^
+// color code the time blocks
+var timeBlocks = [
+    eight,
+    nine,
+    ten,
+    eleven,
+    twelve,
+    one,
+    two,
+    three,
+    four,
+    five,
+]
+console.log(timeBlocks[0].innerText)
+
+for (i=0; i>timeBlocks.length; i++) {
+    if (timeBlocks[i].innerText === currentTime) {
+        timeBlocks[i].addClass(current)
+    } else if (timeBlocks[i].innerText < currentTime) {
+        timeBlocks[i].addClass(upNext)
+    } else {
+        timeBlocks[i].addClass(previous)
+    }
+}
