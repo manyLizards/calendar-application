81791f90d6a72417f14cf894bd5ad3e077c6f888
Jordan <jnbarringer96@gmail.com>
2022-10-12 23:30:59 -0500

    added save buttons to timeblocks

--- script.js
+++ script.js
@@ -22,41 +22,29 @@ var currentTime = currentDate.format("h");
 console.log(eight.innerText)
 
 // color code the time blocks
-// var timeBlocks = [
-//     eight,
-//     nine,
-//     ten,
-//     eleven,
-//     twelve,
-//     one,
-//     two,
-//     three,
-//     four,
-//     five,
-// ]
-// console.log(timeBlocks[0].innerText)
+var timeBlocks = [
+    eight,
+    nine,
+    ten,
+    eleven,
+    twelve,
+    one,
+    two,
+    three,
+    four,
+    five,
+]
+console.log(currentTime)
 
-time = $('.time-block')
-time.each(function() {
-    var hour = parseInt($(this).attr('id'))
-    if (hour === currentTime) {
-    $(this).children('.text').attr('class', 'current text')
-    console.log(('current' + hour + currentTime))
-    } else if (currentTime > hour) {
-        $(this).children('.text').attr('class', 'previous')
+function colorCode () {
+for (i=0; i<timeBlocks.length; i++) {
+    if (timeBlocks[i].innerText == currentTime) {
+        $("form").addClass("current")
+    } else if (timeBlocks[i].innerText < currentTime) {
+        $("form").addClass("upNext")
     } else {
-        $(this.children('.text').attr('class', 'previous'))
+        $("form").addClass("previous")
     }
-})
-
-// function colorCode () {
-// for (i=0; i<timeBlocks.length; i++) {
-//     if (timeBlocks[i].innerText === currentTime) {
-//         $("form").addClass("current")
-//     } else if (timeBlocks[i].innerText < currentTime) {
-//         $("form").addClass("upNext")
-//     } else {
-//         $("form").addClass("previous")
-//     }
-// }
-// }
+}
+}
+colorCode()
