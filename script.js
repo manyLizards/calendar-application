341e3ff8f64c0c0612baad15b88dbadaf30e772f
Jordan <jnbarringer96@gmail.com>
2022-10-12 22:55:53 -0500

    attempted to color code time blocks with another function using jquery script - also not working

--- script.js
+++ script.js
@@ -22,26 +22,41 @@ var currentTime = currentDate.format("h");
 console.log(eight.innerText)
 
 // color code the time blocks
-var timeBlocks = [
-    eight,
-    nine,
-    ten,
-    eleven,
-    twelve,
-    one,
-    two,
-    three,
-    four,
-    five,
-]
-console.log(timeBlocks[0].innerText)
+// var timeBlocks = [
+//     eight,
+//     nine,
+//     ten,
+//     eleven,
+//     twelve,
+//     one,
+//     two,
+//     three,
+//     four,
+//     five,
+// ]
+// console.log(timeBlocks[0].innerText)
 
-for (i=0; i>timeBlocks.length; i++) {
-    if (timeBlocks[i].innerText === currentTime) {
-        timeBlocks[i].addClass(current)
-    } else if (timeBlocks[i].innerText < currentTime) {
-        timeBlocks[i].addClass(upNext)
+time = $('.time-block')
+time.each(function() {
+    var hour = parseInt($(this).attr('id'))
+    if (hour === currentTime) {
+    $(this).children('.text').attr('class', 'current text')
+    console.log(('current' + hour + currentTime))
+    } else if (currentTime > hour) {
+        $(this).children('.text').attr('class', 'previous')
     } else {
-        timeBlocks[i].addClass(previous)
+        $(this.children('.text').attr('class', 'previous'))
     }
-}
+})
+
+// function colorCode () {
+// for (i=0; i<timeBlocks.length; i++) {
+//     if (timeBlocks[i].innerText === currentTime) {
+//         $("form").addClass("current")
+//     } else if (timeBlocks[i].innerText < currentTime) {
+//         $("form").addClass("upNext")
+//     } else {
+//         $("form").addClass("previous")
+//     }
+// }
+// }
